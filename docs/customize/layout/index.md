292f9f59709bee2e7a562c55414320de0f5dd440
Yuuto <55290516+JustYuuto@users.noreply.github.com>
2025-04-12 16:30:59 +0200

    feat: "layout" page

---
+++ docs/customize/layout/index.md
@@ -0,0 +1,23 @@
+---
+title: Layout
+---
+
+You can customize the layout of your profile with different styles.
+
+:::note
+
+To change the layout, you need to be Premium.
+
+:::
+
+## Default
+
+As its name suggests, this is the default layout.
+
+![Default layout](./default.png)
+
+## Modern
+
+This layout is more modern and has a cleaner look.
+
+![Modern layout](./modern.png)
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,15 +11,8 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://help.miwa.lol',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-  trailingSlash: true,
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'miwalol', // Usually your GitHub org/user name.
-  projectName: 'help', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
