cfe91d6b155e63ecf61efb702489441a8193c4c7
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2026-01-15 20:10:29 +0100

    fix: build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import createMDX from '@next/mdx';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ['mdx', 'ts', 'tsx'],
   images: {
     unoptimized: true,
   },
