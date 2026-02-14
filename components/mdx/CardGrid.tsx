8e5dffe1992e945c1eaf521025640ff8d47d815f
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2026-02-14 18:10:07 +0100

    feat: some updates

--- components/mdx/CardGrid.tsx
+++ components/mdx/CardGrid.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function CardGrid({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4">
       {children}
     </div>
   );
