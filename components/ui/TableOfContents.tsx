26328f3b24282597ea77f0efc52241a874ca481c
Tenshii <55290516+CuteTenshii@users.noreply.github.com>
2026-02-26 15:09:18 +0000

    fix(toc): styles

--- components/ui/TableOfContents.tsx
+++ components/ui/TableOfContents.tsx
@@ -6,7 +6,7 @@ export default function TableOfContents({ contents }: {contents: TocElement[]}) 
   }
 
   return (
-    <aside className="sticky top-20 hidden max-h-96 min-w-64 max-w-64 overflow-x-hidden md:block">
+    <aside className="sticky top-20 ml-8 hidden max-h-96 min-w-64 max-w-64 overflow-x-hidden md:block">
       <span className="mb-1 block font-bold">On this page</span>
 
       <ul className="space-y-1">
@@ -14,7 +14,7 @@ export default function TableOfContents({ contents }: {contents: TocElement[]}) 
           return (
             <li key={c.id} className="truncate">
               <a
-                href={`#${c.id}`} title={c.title} className="truncate" style={{ paddingLeft: `${(c.level-2)}rem` }}
+                href={`#${c.id}`} title={c.title} className="truncate" style={{ paddingLeft: `${c.level - 2}rem` }}
               >{c.title}</a>
             </li>
           );
